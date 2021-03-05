7fa5275352488dad24ff8318a3389e680fbe55bf
Daniil Demichev <daniil_demichev@mail.ru>
2021-03-05 16:04:40 +0300

    Вторая часть

--- src/main/java/second_part/HashMapOpen.java
+++ src/main/java/second_part/HashMapOpen.java
@@ -1,19 +1,13 @@
 package second_part;
 
-// Java program to demonstrate implementation of our
-// own hash table with chaining for collision detection 
 import java.util.ArrayList;
 
-// A node of chains 
 class HashNode<K, V>
 {
     K key;
     V value;
-
-    // Reference to next node 
     HashNode<K, V> next;
 
-    // Constructor 
     public HashNode(K key, V value)
     {
         this.key = key;
@@ -21,16 +15,10 @@ class HashNode<K, V>
     }
 }
 
-// Class to represent entire hash table 
 class HashMapOpen<K, V>
 {
-    // bucketArray is used to store array of chains 
     private ArrayList<HashNode<K, V>> bucketArray;
-
-    // Current capacity of array list 
     private int numBuckets;
-
-    // Current size of array list 
     private int size;
 
     public ArrayList<HashNode<K, V>> getBucketArray() {
@@ -41,15 +29,12 @@ class HashMapOpen<K, V>
         this.bucketArray = bucketArray;
     }
 
-    // Constructor (Initializes capacity, size and
-    // empty chains. 
     public HashMapOpen()
     {
         bucketArray = new ArrayList<>();
         numBuckets = 13;
         size = 0;
 
-        // Create empty chains 
         for (int i = 0; i < numBuckets; i++)
             bucketArray.add(null);
     }
@@ -57,8 +42,6 @@ class HashMapOpen<K, V>
     public int size() { return size; }
     public boolean isEmpty() { return size() == 0; }
 
-    // This implements hash function to find index 
-    // for a key 
     private int getBucketIndex(K key)
     {
         int hashCode = key.hashCode();
@@ -66,36 +49,24 @@ class HashMapOpen<K, V>
         return index;
     }
 
-    // Method to remove a given key 
     public V remove(K key)
     {
-        // Apply hash function to find index for given key 
         int bucketIndex = getBucketIndex(key);
 
-        // Get head of chain 
         HashNode<K, V> head = bucketArray.get(bucketIndex);
 
-        // Search for key in its chain 
         HashNode<K, V> prev = null;
         while (head != null)
         {
-            // If Key found 
             if (head.key.equals(key))
                 break;
-
-            // Else keep moving in chain 
             prev = head;
             head = head.next;
         }
-
-        // If key was not there 
         if (head == null)
             return null;
-
-        // Reduce size 
         size--;
 
-        // Remove key 
         if (prev != null)
             prev.next = head.next;
         else
@@ -104,14 +75,11 @@ class HashMapOpen<K, V>
         return head.value;
     }
 
-    // Returns value for a key 
     public V get(K key)
     {
-        // Find head of chain for given key 
         int bucketIndex = getBucketIndex(key);
         HashNode<K, V> head = bucketArray.get(bucketIndex);
 
-        // Search key in chain 
         while (head != null)
         {
             if (head.key.equals(key))
@@ -119,18 +87,14 @@ class HashMapOpen<K, V>
             head = head.next;
         }
 
-        // If key not found 
         return null;
     }
 
-    // Adds a key value pair to hash 
     public void add(K key, V value)
     {
-        // Find head of chain for given key 
         int bucketIndex = getBucketIndex(key);
         HashNode<K, V> head = bucketArray.get(bucketIndex);
 
-        // Check if key is already present 
         while (head != null)
         {
             if (head.key.equals(key))
@@ -141,15 +105,12 @@ class HashMapOpen<K, V>
             head = head.next;
         }
 
-        // Insert key in chain 
         size++;
         head = bucketArray.get(bucketIndex);
         HashNode<K, V> newNode = new HashNode<K, V>(key, value);
         newNode.next = head;
         bucketArray.set(bucketIndex, newNode);
 
-        // If load factor goes beyond threshold, then 
-        // double hash table size 
         if ((1.0*size)/numBuckets >= 0.7)
         {
             ArrayList<HashNode<K, V>> temp = bucketArray;
@@ -169,6 +130,4 @@ class HashMapOpen<K, V>
             }
         }
     }
-
-
 } 
